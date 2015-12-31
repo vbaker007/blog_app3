11eb34b6ade558543f64e72e41c5c91d71536cc5
vbaker007 <vbaker007@hotmail.com>
2015-12-31 16:27:45 -0500

    Update Specs for Creating Article Test

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -8,6 +8,10 @@ RSpec.feature "Creating Articles" do
 
     fill_in "Title", with: "Creating first article"
     fill_in "Body", with: "Lorem Ipsum"
+    click_button "Create Article"
+
+    expect(page).to have_content("Article has been created")
+    expect(page.current_path).to eq(articles_path)
 
   end
 end
