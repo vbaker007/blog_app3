ccd5f839683491598296e4b779d3e83c59c861bf
vbaker007 <vbaker007@hotmail.com>
2016-01-09 02:51:46 -0500

    Update Spec/feature/showing_article.rb file

--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -6,5 +6,13 @@ RSpec.feature "Showing an Article" do
     @article = Article.create(title: "The first article", body: "Body of first article")
   end
 
-  
+  scenario "Display individual article" do
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+  end
 end
