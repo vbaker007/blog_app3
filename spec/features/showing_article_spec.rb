cb4555790fe4b368d6a9510d5912aa3ff4413bca
vbaker007 <vbaker007@hotmail.com>
2016-01-17 20:38:46 -0500

    Limit Editing & Deleting Capability in Views/Articles/show.html.erb file

--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -3,10 +3,36 @@ require 'rails_helper'
 RSpec.feature "Showing an Article" do
 
   before do
-    @article = Article.create(title: "The first article", body: "Body of first article")
+    @john = User.create(email: "john@example.com", password: "password")
+    @fred = User.create(email: "fred@example.com", password: "password")
+    @article = Article.create(title: "The first article", body: "Body of first article", user: @john)
   end
 
+  scenario "A non-signed in user does not Edit or Delete links"
+
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "A non-owner signed in cannot see both links" do
+    login_as(@fred)
+
+    visit "/"
+
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+    
   scenario "Display individual article" do
+    
     visit "/"
 
     click_link @article.title
