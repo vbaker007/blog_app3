8f4d16308b60c1ac04fbe0de9a4c28a49a564ad3
vbaker007 <vbaker007@hotmail.com>
2016-01-25 19:31:36 -0500

    Recent Front-End Renditions & Heroku Deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'articles/index'
-
+  get 'welcome/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
    root 'articles#index'
+   #root 'welcome#index'
 
    resources :articles do
     resources :comments
